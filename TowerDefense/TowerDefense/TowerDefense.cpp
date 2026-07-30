a2314b306162501784f11d704a240e62af0405d2
tjdwns1314 <tjdwns1314@gmail.com>
2026-07-30 10:32:40 +0900

    [Commit] Setting WindowSize

--- TowerDefense/TowerDefense/TowerDefense.cpp
+++ TowerDefense/TowerDefense/TowerDefense.cpp
@@ -127,8 +127,21 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
+ /*  HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);*/
+
+   RECT rc = { 0,0,1920,1080 };
+
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+       100,
+       100,
+       rc.right-rc.left,
+       rc.bottom - rc.top,
+       nullptr,
+       nullptr,
+       hInstance,
+       nullptr
+   );
 
    if (!hWnd)
    {
