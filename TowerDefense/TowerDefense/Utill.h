323cab8c8945470827f41fb9bdf09ee43e026930
tjdwns1314 <tjdwns1314@gmail.com>
2026-07-30 18:23:55 +0900

    [Commit] pch.h setting Utill.h setting

---
+++ TowerDefense/TowerDefense/Utill.h
@@ -0,0 +1,21 @@
+#pragma once
+
+enum class RenederLayer
+{
+	BackGround,
+	Enemy,
+	Projectile,
+	Tower,
+	Effect,
+	Mouse,
+	Count
+};
+
+enum class ActorType
+{
+	BackGround,
+	Block,
+	Effect,
+	Tower,
+	Count
+};
